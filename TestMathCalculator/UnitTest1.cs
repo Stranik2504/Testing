9479e2cadcbcac2eb2ee00b3fedef3052b88f3ae
Stranik2504 <58374405+Stranik2504@users.noreply.github.com>
2023-04-30 21:06:22 +0300

    Update UnitTest by add IClassFixture and CalculatorTestData

--- TestMathCalculator/UnitTest1.cs
+++ TestMathCalculator/UnitTest1.cs
@@ -1,42 +1,86 @@
+using System.Collections;
 using Testing;
 
 namespace TestMathCalculator
 {
-    public class UnitTest1
+    public class Data : IDisposable
+    {
+        public MathCalculator MathCalculator { get; set; }
+
+        public Data()
+        {
+            MathCalculator = new MathCalculator();
+        }
+
+        public void Dispose()
+        {
+            // Отчищаем данные, если надо
+        }
+    }
+
+    public class UnitTest1 : IClassFixture<Data>
     {
         private const int Five = 5;
         private const int Three = 3;
 
-        [Fact]
+        private Data _data;
+
+        public UnitTest1(Data data)
+        {
+            _data = data;
+        }
+
+        [Fact(DisplayName = "Sum five and three")]
+        [Trait("First sum test", "Sum")]
         public void Test1()
         {
-            MathCalculator mathCalculator = new MathCalculator();
-            var result = mathCalculator.Sum(Five, Three);
+            var result = _data.MathCalculator.Sum(Five, Three);
             Assert.Equal(8, result);
         }
 
         [Fact]
         public void Test2()
         {
-            MathCalculator mathCalculator = new MathCalculator();
-            var result = mathCalculator.Substract(Five, Three);
+            var result = _data.MathCalculator.Substract(Five, Three);
             Assert.Equal(2, result);
         }
 
         [Fact]
         public void Test3()
         {
-            MathCalculator mathCalculator = new MathCalculator();
-            var result = mathCalculator.Sum(-Five, -Three);
+            var result = _data.MathCalculator.Sum(-Five, -Three);
             Assert.Equal(-8, result);
         }
 
         [Fact]
         public void Test4()
         {
-            MathCalculator mathCalculator = new MathCalculator();
-            var result = mathCalculator.Substract(Five, -Three);
+            var result = _data.MathCalculator.Substract(Five, -Three);
             Assert.Equal(8, result);
+        }
+
+        [Theory(DisplayName = "Test CalculatorTestData")]
+        [ClassData(typeof(CalculatorTestData))]
+        public void Test5(int v1, int v2, int v3)
+        {
+            var result = _data.MathCalculator.Sum(v1, v2);
+            Assert.Equal(v3, result);
+        }
+    }
+
+    public class CalculatorTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 1, 2, 3 };
+            yield return new object[] { 1, -2, -1 };
+            yield return new object[] { -1, -2, -3 };
+            yield return new object[] { -1, 2, 1 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
